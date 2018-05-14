e2cc40fd0b1356856658ec81bd7f9efaa1c4a22b
Armandas Garsva <equals.tinklas@gmail.com>
2018-05-14 23:42:45 +0700

    major bug fixes

--- routes/players.js
+++ routes/players.js
@@ -154,6 +154,7 @@ function calcRatings(callback) {
           match.pvp[index].home.rating = 0;
           match.pvp[index].away.rating = 0;
           if (!item.home.player2 && !item.away.player2) {
+            // Add null checking for player
             let home_player = players.findIndex(x => item.home ? (x._id.toString() === item.home.player._id.toString())
               : (match.home.player && x._id.toString() === match.home.player._id.toString()));
             let away_player = players.findIndex(x => item.away ? (x._id.toString() === item.away.player._id.toString())
@@ -161,14 +162,14 @@ function calcRatings(callback) {
             if (home_player != -1 && away_player != -1) {
               let hp_fp = players[home_player].initial_points + players[home_player].points;
               let ap_fp = players[away_player].initial_points + players[away_player].points;
-              Match.whoWonPvp(item, (home, away) => {
+              Match.whoWonPvp(item, (home, away, draw) => {
                   let WIN_SIDE = home ? 'home' : 'away';
                   let WIN_INDEX = home ? home_player : away_player;
                   let LOSE_INDEX = home ? away_player : home_player;
                   let LOSE_SIDE = home ? 'away' : 'home';
                   let isStronger = hp_fp > ap_fp;
-                  let diff = isStronger ? hp_fp - ap_fp : ap_fp - hp_fp;
                   let OPTION = isStronger ? 'STRONGER_DIFF' : 'WEAKER_DIFF';
+                  let diff = isStronger ? hp_fp - ap_fp : ap_fp - hp_fp;
 
                   config[OPTION].forEach(result => {
                     if (result.max >= diff && result.min <= diff) {
